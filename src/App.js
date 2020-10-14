237769b9431c53161490e85f1a2d7162c0823b32
Jani Inkala <t9inja00@students.oamk.fi>
2020-10-14 16:54:32 +0300

    Authentication doesn't work. All sites, but logged in site not finished.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,11 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import data from './data.json';
-//import Locations from './components/Locations';
 import Marker from './components/Marker';
 import Infopicker from './components/Infopicker';
 import LoggedInHeader from './components/LoggedInHeader';
-//import Finland from './components/Finland';
+import axios from 'axios';
 
-/*function App() {
-  return (
-    <div>
-      <Header/>
-    </div>
-  );
-}*/
 
 class App extends React.Component{
   constructor(props)
@@ -23,15 +15,44 @@ class App extends React.Component{
     this.state = {
       locations: data.locations,
       searchFieldString: "",
-      loggedIn: false
+      usernameString: "",
+      passwordString: "",
+      loggedIn: false,
+      loggingIn: false,
+      registering: false
     }
   }
+
+  
+
 
   onSearchFieldChange = (event) => {
     this.setState({searchFieldString: event.target.value})
   }
 
+  onUsernameChange = (event) => {
+    this.setState({usernameString: event.target.value})
+  }
 
+  onPasswordChange = (event) => {
+    this.setState({passwordString: event.target.value})
+  }
+
+  //user dude, pass 12345
+  ProcessLogin = () => {
+    axios.post('http://localhost:4000/login', 
+                {},
+                { auth: {
+                  username: this.usernameString,
+                  password: this.state.passwordString
+                }})
+      .then(response => {
+        console.log('login success');
+        this.setState({loggedIn: true, loggingIn: false})
+        
+      })
+      .catch(error => console.log(error));
+  }
 
   render()
   {
@@ -41,7 +62,7 @@ class App extends React.Component{
       <Header />
       <Marker locations={this.state.locations.filter((location) => location.name.includes(this.state.searchFieldString))}/>
       <Infopicker locations={this.state.locations.filter((location) => location.name.includes(this.state.searchFieldString))}/>
-      <button onClick={() => this.setState({loggedIn: !this.state.loggedIn})} className="login">Login</button>
+      <button onClick={() => this.setState({loggingIn: true})} className="login">Login</button>
       </>
       
       if(this.state.loggedIn){
@@ -52,6 +73,26 @@ class App extends React.Component{
         <Marker locations={this.state.locations.filter((location) => location.name.includes(this.state.searchFieldString))}/>
         <Infopicker locations={this.state.locations.filter((location) => location.name.includes(this.state.searchFieldString))}/>
         <button onClick={() => this.setState({loggedIn: !this.state.loggedIn})} className="login">Log out</button>
+        </>
+      }
+      if(this.state.loggingIn){
+        view = 
+        <>
+        <input type="text" className="username" onChange={this.onUsernameChange} value={this.state.usernameString}/>
+        <input type="text" className="password" onChange={this.onPasswordChange} value={this.state.passwordString}/>
+        
+        <button onClick={this.ProcessLogin} className="log_in">Log in</button>
+        <button onClick={() => this.setState({loggedIn: false, loggingIn: false, registering: true})} className="register">Register</button>
+        </>
+      }
+      if(this.state.registering){
+        view = 
+        <>
+        <input type="text" className="username"/>
+        <input type="text" className="password"/>
+        <input type="text" className="email"/>
+        
+        <button onClick={() => this.setState({loggedIn: false, loggingIn: false, registering: false})} className="register">Register</button>
         </>
       }
       return (
--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,74 +1,9 @@
-/*import React from 'react'
-
-const Locations = () => {
-    return(
-        <div>
-
-            <div className="oulu" />
-            <div className="rovaniemi" />
-            <div className="kokkola" />
-            <div className="vaasa" />
-            <div className="kuopio" />
-            <div className="joensuu" />
-            <div className="varkaus" />
-            <div className="jyväskylä" />
-            <div className="tampere" />
-            <div className="pori" />
-            <div className="rauma" />
-            <div className="uusikaupunki" />
-            <div className="lahti" />
-            <div className="turku" />
-            <div className="espoo" />
-            <div className="helsinki" />
-            <div className="loviisa" />
-            <div className="hamina" />
-            <div className="kotka" />
-            <div className="ivalo" />
-        
-        </div>            
-        
-    )
-}
-
-export default Locations;*/
-
-
-
-/*import React from 'react'
-
-export default function Locations() {
-    return (
-        <div>
-            <div className="oulu" />
-            <div className="rovaniemi" />
-            <div className="kokkola" />
-            <div className="vaasa" />
-            <div className="kuopio" />
-            <div className="joensuu" />
-            <div className="varkaus" />
-            <div className="jyväskylä" />
-            <div className="tampere" />
-            <div className="pori" />
-            <div className="rauma" />
-            <div className="uusikaupunki" />
-            <div className="lahti" />
-            <div className="turku" />
-            <div className="espoo" />
-            <div className="helsinki" />
-            <div className="loviisa" />
-            <div className="hamina" />
-            <div className="kotka" />
-            <div className="ivalo" />
-
-        </div>            
-            
-    )
-} */
-
 import React from 'react'
 
 
 export default function Locations(props) {
+
+    
     return (
         <div>
             <div className={[props.name]}></div>
